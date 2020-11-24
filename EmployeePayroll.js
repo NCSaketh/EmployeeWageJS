15ba55f6d9b70d0f2786bd80d57b7f0ca304b50f
NCSaketh <nc.saketh1@gmail.com>
2020-11-24 17:43:14 +0530

    UC12 : Committing the problem done in class

---
+++ EmployeePayroll.js
@@ -0,0 +1,36 @@
+class EmployeePayrollData {
+    // properties
+    id;
+    salary;
+    gender;
+    startDate;
+
+    //constructor
+    constructor(...params) {
+        this.id = params[0];
+        this.name = params[1];
+        this.salary = params[2];
+        this.gender = params[3];
+        this.startDate = params[4]; 
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(name) {
+        this._name = name;
+    }
+
+    toString() {
+        const options = { year: 'numeric', month: 'long', day: 'numeric'}
+        const empDate = this.startDate === undefined ? "undefined" 
+                            : this.startDate.toLocaleDateString("en-US", options);
+        return "id: " + this.id + ", name: " + this.name + ", salary: " + this.salary
+            +  ", gender: " + this.gender + ", startDate: " + empDate;
+    }
+}
+let employeePayrollData = new EmployeePayrollData(1, "Mark", 20000);
+console.log(employeePayrollData.toString());
+let employeePayrollData2 = new EmployeePayrollData(2, "Teresa", 50000, "F", new Date());
+console.log(employeePayrollData2.toString());
