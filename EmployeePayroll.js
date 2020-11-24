94d8eebddc72923beb74316ad0371802f9e07ab5
NCSaketh <nc.saketh1@gmail.com>
2020-11-24 17:53:27 +0530

    Pin UC1: Should have 6 digits

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -1,9 +1,6 @@
 class EmployeePayrollData {
     // properties
-    id;
     salary;
-    gender;
-    startDate;
 
     //constructor
     constructor(...params) {
@@ -14,16 +11,43 @@ class EmployeePayrollData {
         this.startDate = params[4]; 
     }
 
-    get name() {
-        return this._name;
-    }
+    // getter and setter
+    get id() {return this._id;}
+    get name() {return this._name;}
+    get gender() {return this._gender;}
+    get startDate() {return this._startDate;}
 
+    set id(id){
+        let idRegex = RegExp('^[1-9][0-9]*$');
+        if(idRegex.test(id)) this._id = id;
+        else throw "ID is incorrect";
+    }
     set name(name) {
         let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
-        if(nameRegex.test(name))
-            this._name = name;
-        else
-            throw "Name is incorrect!";
+        if(nameRegex.test(name)) this._name = name;
+        else throw "Name is incorrect";
+    }
+    set salary(salary){
+        let salaryRegex = RegExp('^[1-9][0-9]*$');
+        if(salaryRegex.test(salary)) this._salary = salary;
+        else throw "ID is incorrect";
+    }
+    set gender(gender){
+        let genderRegex = RegExp("^[MF]$");
+        if(genderRegex.test(gender)) this._gender = gender;
+        else throw "Gender is incorrect";
+    }
+    set startDate(startDate){
+        let currentDate = new Date();
+        if(startDate <= currentDate) this._startDate = startDate;
+        else throw "Start Date is incorrect";
+    }
+    set pin(pin){
+        // Pin length should be 6
+        let pinLengthRegex = RegExp("^[0-9]{6}$")
+        if(pinLengthRegex.test(pin))
+            this._pin = pin;
+        else throw "Pin should have 6 digits";
     }
 
     toString() {
@@ -34,12 +58,51 @@ class EmployeePayrollData {
             +  ", gender: " + this.gender + ", startDate: " + empDate;
     }
 }
-let employeePayrollData = new EmployeePayrollData(1, "Mark", 20000);
-console.log(employeePayrollData.toString());
-try {
-    employeePayrollData.name = "john";
-} catch(e) {
-    console.error(e);
+
+let employeePayrollData=new EmployeePayrollData(1, "Mark", 3000, "M", new Date());
+process.stdout.write(employeePayrollData.toString()+"\n");
+let employeePayrollData2=new EmployeePayrollData(1,"Terrisa",30000,"F",new Date());
+    process.stdout.write(employeePayrollData2.toString()+"\n");
+
+// Name Validation
+try{
+    employeePayrollData.name="jon";
+    process.stdout.write(employeePayrollData.toString()+"\n");
+}catch(exception){
+    console.error(exception);
 }
-let employeePayrollData2 = new EmployeePayrollData(2, "Teresa", 50000, "F", new Date());
-console.log(employeePayrollData2.toString());
+
+// Id Validation: Should be greater than 0
+try{
+    employeePayrollData2.id = 0;
+}catch(exception){
+    console.error(exception);
+}
+
+// Salary Validation: Should be greater than 0
+try{
+    employeePayrollData2.salary = 0;
+}catch(exception){
+    console.error(exception);
+}
+
+// Gender Validation: Should be M or F
+try{
+    employeePayrollData2.gender = "X";
+}catch(exception){
+    console.error(exception);
+}
+
+// Start Date Validation: Should not be future date
+try{
+    employeePayrollData2.startDate = new Date("2024-6-26");
+}catch(exception){
+    console.error(exception);
+}
+
+// Pin Validation: Should have six characters
+try{
+    employeePayrollData2.pin = "25334";
+}catch(exception){
+    console.error(exception);
+}
